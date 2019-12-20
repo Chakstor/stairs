93026b48ac0f5c932b8713ee027c225b9df3f10c
Carlos Mateo <cmc2k5@gmail.com>
2019-12-20 18:31:37 +0100

    Add button to start game

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 window.onload = () => {
     let button = document.getElementById('start');
 
-    const game = new Game();
-    game.init();
+    button.addEventListener('click', (e) => {
+        const game = new Game();
+              game.init();
 
-    // button.addEventListener('click', (e) => {
-    //     const game = new Game();
-    //           game.init();
-
-    //     button.style.display = "none";
-    // });
+        button.style.display = "none";
+    });
 }
 
